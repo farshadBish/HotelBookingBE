64cd3a2d41ad431cbeeab71a7fe44e5efbcbc2bd
farshadBish <farshadota@gmail.com>
2022-12-06 21:22:20 +0100

    update

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import cors from "cors"
 import { forbiddenErrorHandler, genericErroHandler, notFoundErrorHandler, unauthorizedErrorHandler } from "./errorHandlers.js"
 import usersRouter from "./api/users/index.js"
 import roomsRouter from "./api/bookedRooms/index.js"
+import createHttpError from "http-errors"
 
 
 const server = express()
@@ -12,7 +13,18 @@ const port = process.env.PORT || 3001
 
 // MiddleWares
 
-server.use(cors())
+const whitelist = [process.env.FE_DEV_URL,process.env.FE_PROD_URL]
+
+server.use(cors({origin : (origin,corsNext) => {
+  console.log("ORIGIN: ", origin);
+  if (!origin || whitelist.indexOf(origin)!== -1) {
+
+    corsNext(null,true)
+
+  } else {
+    corsNext(createHttpError(400, "Cors Error!"))
+  }
+}}))
 server.use(express.json())
 
 // EndPoints
@@ -29,7 +41,7 @@ server.use(notFoundErrorHandler)
 server.use(genericErroHandler)
 // Mongoose
 
-mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+mongoose.connect("mongodb+srv://farshadbishomar:Feriember04@cluster0.furml.mongodb.net/HotelBooking?retryWrites=true&w=majority")
 
 mongoose.connection.on("connected", () => {
     console.log("Mongo Connected!")
